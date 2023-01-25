b8316f70062f04c2c998cb4c5a1ab9b8eb196088
RyoutaKaneko <k021g1476@g.neec.ac.jp>
2023-01-25 11:03:47 +0900

    02_01

--- main.cpp
+++ main.cpp
@@ -1,29 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <iostream>
+#include <functional>
 
-//2つの値を比べるMin関数
-template <typename T>
-T Min(T a, T b) {
-	//値を比較
-	if (a <= b) {
-		return static_cast<T>(a);
-	}	
-	return static_cast<T>(b);
-}
+int main(void) {
 
-//例外(char型)
-template< >
-char Min(char a, char b) {
-	printf("数字は代入出来ません。\n");
+	int number = 0;
+	int waitTime = 3;
+
+	srand(time(nullptr));
+	int dice = rand() % 6 + 1;
+
+	//予想を入力
+	printf("奇数だと思ったら1を、偶数だと思ったら0を入力してください\n");
+	scanf_s("[ %d ]", &number);
+
+	// 抽選開始
+	std::function<void()> diceDrawing = [=]() {(dice % 2 == number) ? printf("あたり\n") : printf("はずれ\n"); };
+
+	// 待ち時間関数
+	std::function<void(std::function<void()>, int)> SetTimeout = [](std::function<void()> fx, int waitTime) {
+		Sleep(waitTime * 1000); fx();
+	};
+
+	SetTimeout(diceDrawing, waitTime);
+
 	return 0;
 }
 
-//ここからメイン
-int main() {
-	//関数を使用して値を代入
-	printf("%d\n", Min<int>(1, 4));
-	printf("%f\n", Min<float>(1.1f, 4.1f));
-	printf("%lf\n", Min<double>(5.0f, 2.2f));
-	Min<char>('a', 'b');
-
-	return 0;
-}
